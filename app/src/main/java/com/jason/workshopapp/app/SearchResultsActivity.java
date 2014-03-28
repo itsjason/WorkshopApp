464ce299b4e1e82d73739f24f5314957378db2f8
Jason Norris <Jason.Norris@FCSAmerica.com>
2014-03-28 15:02:21 -0500

    A whole lot of changes

--- app/src/main/java/com/jason/workshopapp/app/SearchResultsActivity.java
+++ app/src/main/java/com/jason/workshopapp/app/SearchResultsActivity.java
@@ -4,39 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class SearchResultsActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final TextView awesomeText = (TextView) findViewById(R.id.awesome_text);
-        Button heresTheButton = (Button) findViewById(R.id.search_button);
-
-        heresTheButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                searchTwitter();
-            }
-        });
-
+        setContentView(R.layout.activity_search_results);
     }
 
-    private void searchTwitter() {
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.search_results, menu);
         return true;
     }
 
